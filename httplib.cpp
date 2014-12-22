2e38b14acff8200de57fcb287be88cdd18fa8dbb
susu <u1240976@gmail.com>
2014-12-22 20:40:54 +0800

    nl2br add \r, \r\n replacing support

--- httplib.cpp
+++ httplib.cpp
@@ -2,7 +2,17 @@
 
 void nl2br(std::string& str){
     std::string::size_type pos = 0;
+    while ((pos = str.find("\r\n", pos)) != std::string::npos){
+        str.replace(pos, 2, "<br />");
+    }
+
+    pos = 0;
+    while ((pos = str.find("\r", pos)) != std::string::npos){
+        str.replace(pos, 1, "<br />");
+    }
+    
+    pos = 0;
     while ((pos = str.find("\n", pos)) != std::string::npos){
-        str.replace(pos, 1, "<br>");
+        str.replace(pos, 1, "<br />");
     }
 }
